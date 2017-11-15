69959e363553e30c7c91587739bf09ea2a023680
Benjamin Bollen <ben@simpletoken.org>
2017-11-15 08:46:51 +0530

    telegram: sparky Bot Police

--- telegram/badbot.js
+++ telegram/badbot.js
@@ -2,79 +2,16 @@ const Slimbot = require('slimbot');
 const FS = require('fs');
 const Path = require('path');
 const PM = require('./privateConversation.js');
+const chats = require('./configChats.js');
 
 // rachins bot !!!! is in COMMUNITY
 // const OfficiaLSimpLeToken_bot = "330790032:AAH2l8hR3bw2t3yM_mRalgQKTQty1qH5h1Q";
 // @benjaminbollens_bot  t.me/benjaminbollens_bot
-const BOT_BENJAMINBOLLEN = "438311760:AAEZnwm-sPvkcCBMRlgswO38VNuh_bcODmI";
-const BOT_BENSBADBOT ="453337344:AAErhzC7OgzmSBILge1rrCnMQ1TUwdOTzi0";
+const BOT_BENSBADBOT ="383374001:AAG_yd10lbQ8Vru029CXGf-os6b8Gy9UvQE";
 
 const badbot = new Slimbot(BOT_BENSBADBOT);
 
-// const CHAT_SIMPLETOKEN_COMMUNITY = -1001138398611;
-
-const CHAT_DEBUG_COMMUNITY = -182770788;
-const CHAT_DEBUG_ANNOUNCEMENTS = -1001320680086;
-
-const CHAT_COMMUNITY = CHAT_DEBUG_COMMUNITY;
-const CHAT_ANNOUNCEMENTS = CHAT_DEBUG_ANNOUNCEMENTS;
-
-const CHAT_TYPE_SUPERGROUP = 'supergroup';
-const CHAT_TYPE_GROUP = 'group';
-const CHAT_TYPE_PRIVATE = 'private';
-
 var users = require('./users.json')
-
-// Received a message
-// { message_id: 6,
-//   from: 
-//    { id: 287665506,
-//      is_bot: false,
-//      first_name: 'Benjamin',
-//      last_name: 'Bollen',
-//      username: 'benjaminbollen' },
-//   chat: 
-//    { id: -265214519,
-//      title: 'Announcements',
-//      type: 'group',
-//      all_members_are_administrators: true },
-//   date: 1510691507,
-//   text: 'hi' }
-
-
-// Received a message
-// { message_id: 12114,
-//   from: 
-//    { id: 467280401,
-//      is_bot: false,
-//      first_name: 'daniel',
-//      last_name: 'ttberg',
-//      username: 'danielttberg' },
-//   chat: 
-//    { id: -1001138398611,
-//      title: 'Simple Token Community',
-//      username: 'simpletoken',
-//      type: 'supergroup' },
-//   date: 1510691604,
-//   new_chat_participant: 
-//    { id: 467280401,
-//      is_bot: false,
-//      first_name: 'daniel',
-//      last_name: 'ttberg',
-//      username: 'danielttberg' },
-//   new_chat_member: 
-//    { id: 467280401,
-//      is_bot: false,
-//      first_name: 'daniel',
-//      last_name: 'ttberg',
-//      username: 'danielttberg' },
-//   new_chat_members: 
-//    [ { id: 467280401,
-//        is_bot: false,
-//        first_name: 'daniel',
-//        last_name: 'ttberg',
-//        username: 'danielttberg' } ] }
-
 
 // Make announcement
 function makeAnnoucement(message) {
@@ -90,8 +27,13 @@ function checkUserForBot(message) {
 
 // Register listeners
 badbot.on('message', message => {
-	if (message.chat && message.chat.id == CHAT_COMMUNITY) {
+	if (message.chat) {// && message.chat.id == chats.CHAT_COMMUNITY) {
 	 	badbot.sendMessage(message.chat.id, "Look at this other ICO!!")
+
+	 	// if (message.chat.text == 'leave') {
+	 		console.log("MOVE OUT");
+	 		badbot.leaveChat(message.chat.id);
+	 	// }
 	} 
 
 	console.log("Received a message");
--- telegram/sparkyBotPolice.js
+++ telegram/sparkyBotPolice.js
@@ -9,13 +9,13 @@ const OfficiaLSimpLeToken_bot = "330790032:AAH2l8hR3bw2t3yM_mRalgQKTQty1qH5h1Q";
 // @benjaminbollens_bot  t.me/benjaminbollens_bot
 const BEN_BADBOT_ID = 383374001;
 const BOT_BENJAMINBOLLEN = "438311760:AAEZnwm-sPvkcCBMRlgswO38VNuh_bcODmI";
-// const BOT_BENSBADBOT = "346673403:AAHVb0cQaI2UsQP1Ae34ePuGP6pFIjDL3-Q";
 
 const sparky = new Slimbot(BOT_BENJAMINBOLLEN);
 
-
-var users = require('./users.json');
 var kickedBots = require('./kickedbots.json');
+
+var lastShownScamAlert = 1510713686; // 2:41:26 am UTC  |  Wednesday, November 15, 2017
+const HOLD_OFF_SCAM_ALERT_MIN = 10; // hold off for 5 min between alerts minimum
 
 const scamAlert = '***************************************************\n\n' +
 "THE ONLY PLACE YOU CAN PURCHASE SIMPLE TOKENS IS AT https://sale.simpletoken.org  Please be sure you are logged in using your username and password. IF YOU SAW A DEPOSIT ADDRESS AND YOU WERE NOT LOGGED IN AT https://sale.simpletoken.org -- IGNORE IT, IT IS A SCAM.\n\n" + 
@@ -25,22 +25,16 @@ const scamAlert = '***************************************************\n\n' +
 "The only way to buy Simple Token is at https://sale.simpletoken.org\n\n" +
 "Please refer to official materials only. The Token Sale Registration Guide and the Purchase Guide can be found on https://sale.simpletoken.org\n\n";
 
-// Make announcement
-function makeAnnoucement(message) {
-	return "Join the Official Simple Token announcement channel!\n" +
-		"https://t.me/"+ message.chat.username +"/" + message.message_id
-}
+function seconds_now(){ return Math.floor( Date.now() / 1000 ) }
 
-
-function reportBadBot(message) {
+function reportBadBot(badbot, date) {
 	kickedBots[date] = {badbot, date};
     const json = JSON.stringify(kickedBots, null, 4);
-    sparky.sendMessage(chats.CHAT_REPORT, "kicked bot " + badbot.username +
-    	" with id " + badbot.id);
+    sparky.sendMessage(-227418557, "kicked bot "); //+ badbot.username +
+    	// " with id " + badbot.id);
     FS.writeFile(Path.join(__dirname, 'kickedbots.json'), json, function(err) {
-	    if(err) {
-	        return console.log(err);
-	    }
+	    if(err) throw err;
+	    console.log("Updated kickedbots.json");
     });
 };
 
@@ -62,46 +56,36 @@ function kickIfNewUserIsBot(message) {
 	}
 };
 
-function resetBadbot() {
-	sparky.unbanChatMember(chats.CHAT_DEBUG_COMMUNIT, BEN_BADBOT_ID);
-	sparky.unbanChatMember(chats.CHAT_DEBUG2_COMMUNIT, BEN_BADBOT_ID);
-	sparky.sendMessage(chats.CHAT_REPORT, "unbanned bens badbot.");
+function holdOffScamAlert() {
+	if (lastShownScamAlert + HOLD_OFF_SCAM_ALERT_MIN > seconds_now()) {
+		return true;
+	}
+	return false;
 }
 
 // Register listeners
 sparky.on('message', message => {
 	// Announcement channel
 	if (message.chat && message.chat.id == chats.CHAT_ANNOUNCEMENTS) {
-		// Announcement channel
-		// if (message.message_id) {
-			// const announcement = makeAnnoucement(message);
-		 //  	sparky.sendMessage(CHAT_COMMUNITY, makeAnnoucement(message));
-			// console.log("announced:", announcement);
-			resetBadbot();
-		// }
+		// BotPolice ignores Annnouncement channel for now
+		return;
+	// Simple Token community channel
 	} else if (message.chat) { // && message.chat.id == CHAT_COMMUNITY) {
 	 	badbot = kickIfNewUserIsBot(message);
 	 	if (badbot != undefined) {
-	 		const humansOnly = humansOnlyPolicy(badbot);
-	 		sparky.sendMessage(message.chat.id, humansOnly);
-	 	} else {
-		 	sparky.sendMessage(message.chat.id, "No bots detected.");
-		}
-	} //else if (message.chat && message.chat.type = CHAT_TYPE_PRIVATE) {
+	 		if (!holdOffScamAlert()) {
+		 		const humansOnly = humansOnlyPolicy(badbot);
+		 		sparky.sendMessage(message.chat.id, humansOnly);
+		 		lastShownScamAlert = seconds_now();
+		 	}
+	 	}
 
-	// }
-
-	console.log("Received a message");
-		    console.dir(message);
+	 	if (!holdOffScamAlert()) {
+	 		sparky.sendMessage(message.chat.id, "hello Im a repeating bot;");
+	 		lastShownScamAlert = seconds_now();
+	 	}
+	}
 });
-
-// Because each inline keyboard button has callback data, you can listen for the callback data and do something with them
-sparky.on('callback_query', query => {
-  if (query.data === 'hello') {
-    sparky.sendMessage(query.message.chat.id, 'Hello to you too!');
-  }
-});
-
 
 // Call API
 
