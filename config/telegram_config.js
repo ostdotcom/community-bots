071b5c3149f076d19d4bcaad853141161149743c
rachin <rachin@pepo.com>
2017-12-04 21:31:01 +0530

    Disable pinned message feature for now.

--- config/telegram_config.js
+++ config/telegram_config.js
@@ -30,7 +30,7 @@ const development = {
 
   //REPEATED CHAT PIN MSG ID
   ,REPEATE_PINNED_MSG_TEXT : "Thanks for a successful token sale! The sale has ended. The Simple Token team has provided answers to many of the most common questions. [link]"
-  ,REPEATED_CHAT_PIN_MSG_ID : st_dev_community_1
+  ,REPEATED_CHAT_PIN_MSG_ID : null
   
   ,PUBLIC_COMMUNITY_USERNAME : "stdevcommunity"
 
